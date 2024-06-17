2fc7d42d8dc70959e27d248e0c05cccfc582fab0
Srikanth Rallabandi <codewithsrikanth@hotmail.com>
2024-06-17 20:09:10 +0530

    Add project files.

---
+++ OnelineHub.Repositories/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using OnlineHub.Entities;
+
+namespace OnelineHub.Repositories
+{
+    public class AppDbContext : IdentityDbContext<User,Role,int>
+    {
+        public DbSet<Category>  Categories { get; set; }
+        public DbSet<Item>  Items { get; set; }
+        public DbSet<ItemType>  ItemTypes { get; set; }
+        public DbSet<Order>  Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<Cart> Carts { get; set; }
+        public DbSet<CartItem> CartItems { get; set; }
+        public DbSet<Address> Address { get; set; }
+        public DbSet<PaymentDetails> PaymentDetails { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=.;database=OnlineHubDB;TrustServerCertificate=True;Trusted_Connection=true;");
+        }
+    }
+}
---
+++ OnlineHub.Entities/CartItem.cs
@@ -0,0 +1,20 @@
+﻿namespace OnlineHub.Entities
+{
+    public class CartItem
+    {
+        public CartItem(int itemId,int quantity,decimal unitPrice)
+        {
+            ItemId = itemId;
+            Quantity = quantity;
+            UnitPrice = unitPrice;
+        }
+
+        public int Id { get; set; }
+        public Guid CartId { get; set; }
+        public int ItemId { get; set; }
+        public decimal UnitPrice { get; set; }
+        public int Quantity { get; set; }
+
+        public Cart Cart { get; set; }
+    }
+}
